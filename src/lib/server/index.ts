85df755a88ca3a96f0c2a16a298f36958717a27c
Layendan <layendan.h@gmail.com>
2025-09-08 02:45:50 -0700

    Refresh osu api token when expired

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -4,3 +4,12 @@ import { Auth, Client } from 'osu-web.js';
 const auth = new Auth(parseInt(OSU_CLIENT_ID), OSU_CLIENT_SECRET, '');
 const token = await auth.clientCredentialsGrant();
 export const client = new Client(token.access_token);
+
+async function refreshToken() {
+	const token = await auth.clientCredentialsGrant();
+	client.setAccessToken(token.access_token);
+
+	setTimeout(() => refreshToken(), token.expires_in);
+}
+
+setTimeout(() => refreshToken(), token.expires_in);
