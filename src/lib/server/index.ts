f490eacea263afd55148174835ceb760aa14274e
Layendan <layendan.h@gmail.com>
2025-09-27 16:05:12 -0700

    Testing new fix for token regeneration and prerender front page

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,15 +1,22 @@
 import { OSU_CLIENT_ID, OSU_CLIENT_SECRET } from '$env/static/private';
-import { Auth, Client } from 'osu-web.js';
+import { Auth, Client, isOsuJSError } from 'osu-web.js';
 
 const auth = new Auth(parseInt(OSU_CLIENT_ID), OSU_CLIENT_SECRET, '');
+// If this fails, the whole app breaks
 const token = await auth.clientCredentialsGrant();
 export const client = new Client(token.access_token);
 
 async function refreshToken() {
-	const token = await auth.clientCredentialsGrant();
-	client.setAccessToken(token.access_token);
+	try {
+		const token = await auth.clientCredentialsGrant();
+		client.setAccessToken(token.access_token);
 
-	setTimeout(() => refreshToken(), token.expires_in);
+		setTimeout(() => refreshToken(), token.expires_in - 10);
+	} catch (e) {
+		console.error('Error refreshing token', e);
+		if (isOsuJSError(e)) console.error(`Message: ${e.message}`, `Cause: ${e.cause}`);
+		refreshToken();
+	}
 }
 
-setTimeout(() => refreshToken(), token.expires_in);
+setTimeout(() => refreshToken(), token.expires_in - 10);
--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,7 @@ const config = {
 				'frame-ancestors': ['none'],
 				'upgrade-insecure-requests': true
 			},
-			mode: 'nonce'
+			mode: 'auto'
 		}
 	},
 	compilerOptions: {
