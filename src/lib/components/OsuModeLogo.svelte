6a41c4d8f6be20caefc59efc5fb64cb65aa58bdf
Layendan <layendan.h@gmail.com>
2025-11-17 14:41:39 -0700

    Differentiate svg ids

--- src/lib/components/OsuModeLogo.svelte
+++ src/lib/components/OsuModeLogo.svelte
@@ -4,35 +4,37 @@
 	stroke="currentColor"
 	viewBox="0 0 666.66669 666.66669"
 	xmlns="http://www.w3.org/2000/svg">
-	<defs id="defs1">
-		<clipPath clipPathUnits="userSpaceOnUse" id="clipPath2">
-			<path d="M 0,500 H 500 V 0 H 0 Z" transform="translate(-250,-120)" id="path2"></path>
+	<defs id="osuDiffDefs1">
+		<clipPath clipPathUnits="userSpaceOnUse" id="osuDiffClipPath2">
+			<path d="M 0,500 H 500 V 0 H 0 Z" transform="translate(-250,-120)" id="osuDiffPath2"></path>
 		</clipPath>
-		<clipPath clipPathUnits="userSpaceOnUse" id="clipPath4">
-			<path d="M 0,500 H 500 V 0 H 0 Z" transform="translate(-250,-400.00001)" id="path4"></path>
+		<clipPath clipPathUnits="userSpaceOnUse" id="osuDiffClipPath4">
+			<path d="M 0,500 H 500 V 0 H 0 Z" transform="translate(-250,-400.00001)" id="osuDiffPath4"
+			></path>
 		</clipPath>
-		<clipPath clipPathUnits="userSpaceOnUse" id="clipPath6">
-			<path d="M 0,500 H 500 V 0 H 0 Z" transform="translate(-250,-485.00001)" id="path6"></path>
+		<clipPath clipPathUnits="userSpaceOnUse" id="osuDiffClipPath6">
+			<path d="M 0,500 H 500 V 0 H 0 Z" transform="translate(-250,-485.00001)" id="osuDiffPath6"
+			></path>
 		</clipPath>
 	</defs>
-	<g id="layer-MC1">
+	<g id="osuDiffLayer-MC1">
 		<path
-			id="path1"
+			id="osuDiffPath1"
 			d="m 0,0 c -71.682,0 -130,58.317 -130,130 0,71.682 58.318,130 130,130 71.683,0 130,-58.318 130,-130 C 130,58.317 71.683,0 0,0"
 			style="fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:0.99975002;stroke-dasharray:none"
 			transform="matrix(1.3333333,0,0,-1.3333333,333.33333,506.66667)"
-			clip-path="url(#clipPath2)"></path>
+			clip-path="url(#osuDiffClipPath2)"></path>
 		<path
-			id="path3"
+			id="osuDiffPath3"
 			d="m 0,0 c -82.843,0 -150,-67.157 -150,-150 0,-82.843 67.157,-150 150,-150 82.843,0 150,67.157 150,150 C 150,-67.157 82.843,0 0,0 M 0,-40 C 29.382,-40 57.006,-51.442 77.781,-72.218 98.558,-92.995 110,-120.618 110,-150 110,-179.382 98.558,-207.006 77.781,-227.781 57.006,-248.558 29.382,-260 0,-260 c -29.382,0 -57.005,11.442 -77.782,32.219 -20.776,20.775 -32.218,48.399 -32.218,77.781 0,29.382 11.442,57.005 32.218,77.782 C -57.005,-51.442 -29.382,-40 0,-40"
 			style="fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:0.99975002;stroke-dasharray:none"
 			transform="matrix(1.3333333,0,0,-1.3333333,333.33333,133.33333)"
-			clip-path="url(#clipPath4)"></path>
+			clip-path="url(#osuDiffClipPath4)"></path>
 		<path
-			id="path5"
+			id="osuDiffPath5"
 			d="m 0,0 c -31.707,0 -62.487,-6.219 -91.485,-18.484 -27.989,-11.838 -53.116,-28.777 -74.685,-50.346 -21.569,-21.569 -38.508,-46.697 -50.346,-74.685 C -228.781,-172.513 -235,-203.293 -235,-235 c 0,-31.707 6.219,-62.487 18.484,-91.484 11.838,-27.99 28.777,-53.117 50.346,-74.686 21.569,-21.569 46.696,-38.508 74.685,-50.347 C -62.487,-463.781 -31.707,-470 0,-470 c 31.707,0 62.487,6.219 91.484,18.483 27.99,11.839 53.117,28.778 74.686,50.347 21.569,21.569 38.508,46.696 50.347,74.686 12.264,28.997 18.483,59.777 18.483,91.484 0,31.707 -6.219,62.487 -18.483,91.485 -11.839,27.988 -28.778,53.116 -50.347,74.685 -21.569,21.569 -46.696,38.508 -74.686,50.346 C 62.487,-6.219 31.707,0 0,0 m 0,-40 c 107.695,0 195,-87.305 195,-195 0,-107.695 -87.305,-195 -195,-195 -107.696,0 -195,87.305 -195,195 0,107.695 87.304,195 195,195"
 			style="fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:0.99975002;stroke-dasharray:none"
 			transform="matrix(1.3333333,0,0,-1.3333333,333.33333,20)"
-			clip-path="url(#clipPath6)"></path>
+			clip-path="url(#osuDiffClipPath6)"></path>
 	</g>
 </svg>
